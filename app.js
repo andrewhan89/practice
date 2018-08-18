a76431d1892143b7eac641fd13cf1c55c4865ccc
Andrew Han <andrewhan@Andrews-MacBook-Pro.local>
2018-08-18 09:27:29 -0700

    problem 1 solution

--- app.js
+++ app.js
@@ -5,4 +5,14 @@
  * Find the sum of all the multiples of 3 or 5 below 1000.
  */
 
- 
+function sumOfMultiple35(number) {
+    var sum = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
